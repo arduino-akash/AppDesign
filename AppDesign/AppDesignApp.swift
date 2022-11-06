768bafdabfd1f737a3376fa1e013a2a1b46deeb0
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-06 15:41:36 +0530

    Initial Commit

---
+++ AppDesign/AppDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDesignApp.swift
+//  AppDesign
+//
+//  Created by Singh, Akash | RIEPL on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AppDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
